7100850a8622cabb3bc875bdb235be96a1e05655
Damian Baczyński <224250@edu.p.lodz.pl>
2021-01-31 23:33:04 +0100

    Extends repositories interfaces with new functionality

--- rest/src/main/kotlin/com/github/lupuuss/todo/api/rest/repository/task/TaskRepository.kt
+++ rest/src/main/kotlin/com/github/lupuuss/todo/api/rest/repository/task/TaskRepository.kt
@@ -2,9 +2,9 @@ package com.github.lupuuss.todo.api.rest.repository.task
 
 interface TaskRepository {
 
-    fun findTasksByUser(userId: String)
+    fun findTasksByUser(userId: String): List<TaskData>
 
-    fun findTasksByUserAndStatus(userId: String, status: TaskData.Status)
+    fun findTasksByUserAndStatus(userId: String, status: TaskData.Status): List<TaskData>
 
     fun saveTask(task: TaskData)
 
--- rest/src/main/kotlin/com/github/lupuuss/todo/api/rest/repository/user/UserRepository.kt
+++ rest/src/main/kotlin/com/github/lupuuss/todo/api/rest/repository/user/UserRepository.kt
@@ -2,7 +2,15 @@ package com.github.lupuuss.todo.api.rest.repository.user
 
 interface UserRepository {
 
+    fun findAll(): List<UserData>
+
+    fun findUserByLoginContains(query: String): List<UserData>
+
     fun findUserByLogin(login: String): UserData?
 
     fun findUserById(id: String): UserData?
+
+    fun saveUser(user: UserData)
+
+    fun deleteUser(id: String)
 }
