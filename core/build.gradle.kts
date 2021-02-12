c02e6c3cc0359460aa9b5be8069628b358298cea
Damian Baczyński <224250@edu.p.lodz.pl>
2021-02-12 18:03:11 +0100

    Init publishing in gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
-    kotlin("multiplatform") version "1.4.21"
+    kotlin("multiplatform") version "1.4.30"
+    id("maven-publish")
 }
 
 group = "com.github.lupuuss.todo"
 version = "1.0-SNAPSHOT"
+
+publishing {
+
+    repositories {
+        maven {
+            url = uri("https://mymavenrepo.com/repo/BjwNRjndUUDYeEf55vUU/")
+        }
+    }
+}
 
 kotlin {
 
@@ -11,6 +21,23 @@ kotlin {
 
     js {
         browser()
+    }
+
+    val publicationsFromMainHost =
+        listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+    publishing {
+
+        publications {
+            create<MavenPublication>("todo-api-core") {
+            }
+
+            matching { it.name in publicationsFromMainHost }.all {
+                val targetPublication = this@all
+                tasks.withType<AbstractPublishToMaven>()
+                    .matching { it.publication == targetPublication }
+                    .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+            }
+        }
     }
 
     sourceSets {
